d8f6f61f1ed7e234d8ac6ecebc1a4fe99261a118
王 <513972422@qq.com>
2017-10-24 16:31:38 +0800

    初步创建
    现在支持的四种压缩类型 tar tar.bz2 tar.gz zip

---
+++ bash/tools/decompression.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# one key decompression
+# 一键解压，无须判断类型 现在支持类型有 tar tar.bz2 tar.gz zip
+for i in $@
+do
+  case `file -b "$i"| cut -d , -f1` in
+  "POSIX tar archive (GNU)")
+    tar xvf $i
+  ;;
+  "bzip2 compressed data")
+    tar jxvf $i
+  ;;
+  "gzip compressed data")
+    tar zxvf $i
+  ;;
+  "Zip archive data")
+    unzip $i
+  ;;
+  *)
+    echo "Do not support type" >&2
+  esac
+done
+

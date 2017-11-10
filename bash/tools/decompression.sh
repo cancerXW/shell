68d25a4dbe352530320ea59517682d5b9b9a9b1f
王 <513972422@qq.com>
2017-11-10 20:38:12 +0800

    新增tar.xz 的支持
    新增对tar.xz文件的支持

--- bash/tools/decompression.sh
+++ bash/tools/decompression.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# one key decompression , support type tar tar.bz2 tar.gz zip 
-# 一键解压，无须判断类型 现在支持类型有 tar tar.bz2 tar.gz zip
+# one key decompression , support type tar tar.bz2 tar.gz zip tar.xz
+# 一键解压，无须判断类型 现在支持类型有 tar tar.bz2 tar.gz zip tar.xz
 for i in $@
 do
   case `file -b "$i"| cut -d , -f1` in
@@ -15,7 +15,11 @@ do
   ;;
   "Zip archive data")
     unzip $i
-  ;;
+  ;; 
+  "XZ compressed data")
+      xz $i
+      tar xvf $i
+  ;;  
   *)
     echo "Do not support type" >&2
     exit 2
